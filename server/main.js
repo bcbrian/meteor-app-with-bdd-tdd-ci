5579e75f4e5e7136ed4b467160018d0574503bae
Brian Bartholomew <me@bcbrian.com>
2016-05-31 20:10:40 -0500

    fix server main.js load order bug

--- server/main.js
+++ server/main.js
@@ -1,10 +1,11 @@
 import { Meteor } from "meteor/meteor";
-import { setupMeteorBackDoorMethods } from "meteor/bcbrian:test-helpers"
 
 Meteor.startup(function () {
   const USE_TEST_ENVIRONMENT = process.env.USE_TEST_ENVIRONMENT || false;
   if(USE_TEST_ENVIRONMENT){
     console.log("SETTING UP BACKDOOR METHODS");
-    setupMeteorBackDoorMethods();
+    var fixtures = require('meteor/bcbrian:test-helpers');
+    console.log("FIXTURES: ", fixtures);
+    fixtures.setupMeteorBackDoorMethods();
   }
 });
