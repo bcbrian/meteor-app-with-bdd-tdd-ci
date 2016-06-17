605bf5efe43d20473e8aab09a0b47d73f20c95c0
Brian Bartholomew <me@bcbrian.com>
2016-06-17 08:07:50 -0500

    removed and updated a few logging systems

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -3,9 +3,8 @@ import { Meteor } from "meteor/meteor";
 Meteor.startup(function () {
   const USE_TEST_ENVIRONMENT = process.env.USE_TEST_ENVIRONMENT || false;
   if(USE_TEST_ENVIRONMENT){
-    console.log("SETTING UP BACKDOOR METHODS");
+    console.log("### SETTING UP BACKDOOR METHODS ###");
     var fixtures = require('/imports/test-helpers/test-helpers.js');
-    console.log("FIXTURES: ", fixtures);
     fixtures.setupMeteorBackDoorMethods();
   }
 });
